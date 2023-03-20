44bf16da2eddd525a7055d6f039327cd17dbaa2c
makoto nishizawa <infinite4000@gmail.com>
2023-03-20 14:23:51 +0900

    test

---
+++ 新しいテキスト ドキュメント.py
@@ -0,0 +1,25 @@
+import configparser
+import zipfile
+import os
+import sys
+
+def zipdir(path, zipname):
+    config = configparser.ConfigParser()
+    config.read('setting.ini')
+    password = config.get('Archive', 'password')
+
+    zipf = zipfile.ZipFile(zipname, 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zipf.write(os.path.join(root, file), file, zipfile.ZIP_DEFLATED)
+    zipf.setpassword(password.encode())
+    zipf.close()
+
+if __name__ == '__main__':
+    # コマンドライン引数からフォルダとzipファイル名を取得する
+    args = sys.argv
+    path = args[1]
+    zipname = args[2]
+    
+    # フォルダをzip圧縮する
+    zipdir(path, zipname)
